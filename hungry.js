65c756c32e624dfc122f5b00fea0aa336358fb96
jmmohiuddin <71925444+jmmohiuddin@users.noreply.github.com>
2021-02-07 15:21:45 +0600

    ar vallagena

--- hungry.js
+++ hungry.js
@@ -21,14 +21,15 @@ function getMealList(){
         if(data.meals){
             data.meals.forEach(meal => {
                 html += `
+                    
                     <div class = "meal-item" data-id = "${meal.idMeal}">
                         <div class = "meal-img">
                             <img src = "${meal.strMealThumb}" alt = "food">
                         </div>
                         <div class = "meal-name">
                             <h3>${meal.strMeal}</h3>
-                            <a href = "#" class = "recipe-btn">Get Recipe</a>
-                        </div>
+                             <a href = "#" class = "recipe-btn">Details</a>
+                        </div> 
                     </div>
                 `;
             });
